a1ac35f9fd72f66b12ed38b4b5431f67a61b5827
Nolex01 <noel.szabo02@icloud.com>
2024-02-10 20:05:41 +0100

    implement security to the framework

--- public/index.php
+++ public/index.php
@@ -12,11 +12,38 @@ $configFile = dirname(__DIR__) . '/app/config/config.json';
 $configJson = file_get_contents($configFile);
 $config = json_decode($configJson, true);
 
+$allowedMethods = $config['allowedMethods'] ?? [];
+$allowedIPs = $config['allowedIPs'] ?? [];
+$allowedBrowsers = $config['allowedBrowsers'] ?? [];
+
+$uri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+if (array_key_exists($uri, $allowedMethods) && !in_array($requestMethod, $allowedMethods[$uri])) {
+    http_response_code(405);
+    echo '405 Method Not Allowed';
+    exit;
+}
+
+$clientIP = $_SERVER['REMOTE_ADDR'];
+if (!in_array($clientIP, $allowedIPs)) {
+    http_response_code(403);
+    echo '403 Forbidden';
+    exit;
+}
+
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+$browser = get_browser(null, true)['browser'];
+if (!in_array($browser, $allowedBrowsers)) {
+    http_response_code(403);
+    echo '403 Forbidden';
+    exit;
+}
+
 $routes = [
     '/' => 'HomeController@index',
 ];
 
-$uri = $_SERVER['REQUEST_URI'];
 if (array_key_exists($uri, $routes)) {
     list($controllerName, $action) = explode('@', $routes[$uri]);
     $controllerName = 'App\\Controllers\\' . $controllerName;
