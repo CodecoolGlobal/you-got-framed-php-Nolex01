89942209e766317684edf36fca95cc7766c27126
Nolex01 <noel.szabo02@icloud.com>
2024-02-10 19:33:26 +0100

    implement BladeOne in index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,28 @@
 
 require_once '../vendor/autoload.php';
 
-$configFile = __DIR__ . '/config/config.json';
+use eftec\bladeone\BladeOne;
+
+$viewsPath = dirname(__DIR__) . '/app/Views';
+$cachePath = dirname(__DIR__) . '/cache';
+
+$blade = new BladeOne($viewsPath,$cachePath);
+
+$configFile = dirname(__DIR__) . '/app/config/config.json';
 $configJson = file_get_contents($configFile);
 $config = json_decode($configJson, true);
+
+$uri = $_SERVER['REQUEST_URI'];
+$routes = [
+    '/' => 'HomeController@index',
+];
+
+if (array_key_exists($uri, $routes)) {
+    list($controllerName, $action) = explode('@', $routes[$uri]);
+    $controllerName = 'App\\Controllers\\' . $controllerName;
+    $controller = new $controllerName();
+    $controller->$action();
+} else {
+    http_response_code(404);
+    echo '404 Not Found';
+}
