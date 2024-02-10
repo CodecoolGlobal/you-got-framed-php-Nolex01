9faa729c3fa7d97ed03493a915caf93301c2619c
Nolex01 <noel.szabo02@icloud.com>
2024-02-10 18:52:38 +0100

    require autoload.php in index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once '../vendor/autoload.php';
+
 $configFile = __DIR__ . '/config/config.json';
 $configJson = file_get_contents($configFile);
 $config = json_decode($configJson, true);
